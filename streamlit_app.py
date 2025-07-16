07aa6aa03f2ad6877115494a6cf52c1f99e4e178
diotleib <165886285+diotleib@users.noreply.github.com>
2025-07-16 10:33:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,7 @@ st.title("Hallo Welt!")
 
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 st.header("This is a header with a divider", divider="rainbow")   
+agree = st.checkbox("I agree")
+
+if agree:
+    st.write("Great!")
