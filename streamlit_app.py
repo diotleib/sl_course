e569d9f2a52a4a65b6d21fc5b538b34990481a99
diotleib <165886285+diotleib@users.noreply.github.com>
2025-07-16 10:37:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,8 @@ import streamlit as st
 
 st.title("Hallo Welt!")
 
-with st.sidebar
+with st.sidebar:
     st.header("Über diese App")
-
 
 st.header("_Streamlit_ is :blue[cool] :sunglasses:")
 st.header("This is a header with a divider", divider="rainbow")   
